f11e38a2884aa15e9a2fecc753d1cecb80f31d0e
Kailash Turimella <kailash.turimella@gmail.com>
2020-08-24 11:27:24 -0700

    Update Kernel Principal Component Analysis.py

--- Kernel Principal Component Analysis.py
+++ Kernel Principal Component Analysis.py
@@ -10,7 +10,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
 # Importing datasets
 dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:,[2,3]].values
